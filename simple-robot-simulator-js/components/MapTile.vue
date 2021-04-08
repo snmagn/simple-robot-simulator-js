c81f833096ce329521751784c6210cbc8ac66742
snmagn <fulladder6265@gmail.com>
2021-04-08 13:31:43 +0000

    もうちょっとマップっぽく

--- simple-robot-simulator-js/components/MapTile.vue
+++ simple-robot-simulator-js/components/MapTile.vue
@@ -17,8 +17,14 @@ export default {
     bgColor: function () {
       // `this` は vm インスタンスを指します
       var bgColors = {
+        // スタート
+        0: "rgb(44, 200, 200)",
+        // 壁
         1: "rgb(44, 44, 44)",
+        // 床
         2: "#eee",
+        // ゴール
+        9: "rgb(200, 44, 44)",
       }
       return {
         '--back-ground': bgColors[this.type],
